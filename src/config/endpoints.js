d9e29d6a1bd1d3a8ca3f27cb7460715207f0911e
Sergey V. Kior <svkior@gmail.com>
2017-09-25 21:20:15 +0300

    Updated Yarn lockfile

---
+++ src/config/endpoints.js
@@ -0,0 +1,1 @@
+export const relayApi = 'https://api.graph.cool/relay/v1/cj80g2yif0dyj0106vtmeu7q8'
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Router, browserHistory} from 'react-router'
+
+import Routes from './routes'
 
 ReactDOM.render(
-    <div>
-        <h1>Hello world!</h1>
-    </div>
+    <Router
+        history={browserHistory}
+        routes={Routes}>
+    </Router>
     
     , document.getElementById('root'));
